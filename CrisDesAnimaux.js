af743f5efed6ea28eb04ee60938bf692a8b886f2
halimgambit <70286759+halimgambit@users.noreply.github.com>
2024-02-26 14:23:00 +0100

    Update CrisDesAnimaux.js

--- CrisDesAnimaux.js
+++ CrisDesAnimaux.js
@@ -11,8 +11,20 @@ exports.action = function(data, callback){
 function crisAnimal (data, client) {
 
 const fs = require('fs');
+const path = require('path');
 
-let animalName = data.action.rawSentence.toLowerCase().replace("le cri d'animal", "").replace("le cri de l'animal", "").replace("l'", "").replace("d'", "").replace("le", "").replace("animal", "").replace("de", "").replace("la", "").replace("du", "").replace("2", "").trim();
+let animalName = data.action.rawSentence.toLowerCase()
+    .replace("le cri d'animal", "")
+    .replace("le cri de l'animal", "")
+    .replace("l'", "")
+    .replace("d'", "")
+    .replace("le", "")
+    .replace("animal", "")
+    .replace("de", "")
+    .replace("la", "")
+    .replace("du", "")
+    .replace("2", "")
+    .trim();
 
 if (!animalName) {
     Avatar.speak(`Je n'ai pas compris le cri de l'animal que tu veux.`, data.client, () => {
@@ -21,13 +33,16 @@ if (!animalName) {
     return;
 }
 
-if (animalName) {
-	const animalFilePath = `${__dirname}/animaux/${animalName.replace("é", "e").replace("è", "e")}.mp3`;
+const sanitizedAnimalName = animalName.replace(/é/g, 'e').replace(/è/g, 'e');
+const animalFilePath = path.join(__dirname, 'animaux', `${sanitizedAnimalName}.mp3`);
+
+if (fs.existsSync(animalFilePath)) {
     Avatar.speak(`Je mets le cri ${animalName}`, data.client, () => {
         Avatar.Speech.end(data.client, true, () => {
             Avatar.play(animalFilePath, data.client);
         });
     });
+	return;
 } else {
     Avatar.speak(`Le cri de l'animal ${animalName} n'est pas disponible.`, data.client, () => {
         Avatar.Speech.end(data.client);
